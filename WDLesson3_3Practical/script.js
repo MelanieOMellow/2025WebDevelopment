a1710bd4eb77c5c2a509dc5d981028966c690cdc
Mellow <Melanieo45@nycstudents.net>
2026-01-08 14:02:25 +0000

    update

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,5 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+
+function lamb1 (){ 
+    document.getElementsById("Lamb 1 ").InnerHTML = "Lamb is swaying side to side :D"
+}
--- WDLesson3_4Practical/script.js
+++ WDLesson3_4Practical/script.js
@@ -16,10 +16,23 @@ function createStory(){
 
   Choose your variable names wisely.  This is a lot of practice of creating variables and using document.getElementById(..)
   */
-
+ function createStory() { 
+  let boyName = document.getElementById("boyname");
+  let vegetable1 = document.getElementById("veggie");
+  let girlName = document.getElementById("girlname");
+  let vegetable2 = document.getElementById("avegi");
+ 
+ }
   /* Task 4: Set the innerHTML for each of the spans on lines 3 to 9 with the appropriate values from the variables you create in Task 3. */
 
+boy1span1.innerHTML = boyName.value;
+boy1span2.innerHTML = vegetable1.value;
+boy1span3.innerHTML = girlName.value;
+boy1span4.innerHTML = vegetable2.value;
+veg1.innerHTML = vegetable1.value;
+veg2.innerHTML = vegetable2.value;
+girlspan.innerHTML = girlName.value;
+
   /* Task 5: Set the display property of the element with an id of "story" to 'block' */
-
-
-}
+  
+  document.getElementById("story").style.display = 'block';
