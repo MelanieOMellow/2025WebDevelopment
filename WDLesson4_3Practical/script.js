73bdaf05009255177503594b92ab8a59ab8c04c3
Mellow <Melanieo45@nycstudents.net>
2026-03-04 14:00:15 +0000

    update

--- WDLesson4_3Practical/script.js
+++ WDLesson4_3Practical/script.js
@@ -1,21 +1,40 @@
 /* Challenge 2: Create a function to ,
        1) Calculate and display the BMI.  Look up the formula which you should have done in Challenge 1
        2) Display BMI status as per the chart in index.html
-       3) Display the corresponding image based on the BMI status.
-
-       
+       3) Display the corresponding image based on the BMI status.*/
 
 
-       
-*/
-/* 
-Guideline:
+
+function bmi(){}
+  let H = parseFloat(document.getElementById("H").value);
+  let W = parseFloat(document.getElementById("W").value);
+  let output = document.getElementById("output");
+
+  let msg = ""
+  let filename = ""
+
+
+  let BMI = 703 * ( W / H ** 2 );
+  if (BMI < 18.5){
+       msg = "UnderWeight!"
+
+  }else if (BMI >= 18.4 && BMI <= 24.9 ){
+       msg = "Heavy Weight"
+       filename = "healthyweight.png";
+  }
+
+  output.innerHTML= `your BMI is ${BMI}. <img src = ${filename}>`
+
+
+
+
+
+/* Guideline
 1) Create the function
 2) Get the information from the UI
 3) Perform the necessary calculation.
 4) Make the appropriate decisions.  Store the appropriate BMI status.  Store the appropriate filename for the image.
 5) Display the output
-
 Use past examples, classwork and practicals to assist you in completing this practical.
 */
 
